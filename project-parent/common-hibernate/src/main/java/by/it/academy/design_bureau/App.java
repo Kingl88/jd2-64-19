84553e2215a9d931a231a7cb7bbd3817d2208459
Kingl88 <King1375698l>
2020-01-18 23:36:13 +0300

    hibernate Person

--- project-parent/common-hibernate/src/main/java/by/it/academy/design_bureau/App.java
+++ project-parent/common-hibernate/src/main/java/by/it/academy/design_bureau/App.java
@@ -1,5 +1,6 @@
 package by.it.academy.design_bureau;
 
+import by.it.academy.design_bureau.DAO.PersonDAO;
 import by.it.academy.design_bureau.entity.Person;
 import by.it.academy.design_bureau.util.HibernateUtil;
 import org.hibernate.Session;
@@ -11,14 +12,13 @@ public class App {
     public static void main(String[] args) {
         Session session = HibernateUtil.getSessionFactory().openSession();
         session.beginTransaction();
-
-        Person person = new Person(null, "Ivan", "Ivanov", 33);
-
-        session.save(person);
-
-        person.setName("new name");
-        session.getTransaction().commit();
-
+        Person person = new Person(null, "hjhkhj", "gdfgd", 45);
+        PersonDAO.create(person);
+        PersonDAO.read((long) 1);
+        Person person1 = new Person(null, "dfdfdsf", "dgfdsgds", 15);
+        PersonDAO.update(person1, (long)1);
+        PersonDAO.read((long)1);
+        PersonDAO.delete((long)1);
         HibernateUtil.shutdown();
 
     }
---
+++ project-parent/common-hibernate/src/main/java/by/it/academy/design_bureau/DAO/PersonDAO.java
@@ -0,0 +1,49 @@
+package by.it.academy.design_bureau.DAO;
+
+import by.it.academy.design_bureau.entity.Person;
+import by.it.academy.design_bureau.util.HibernateUtil;
+import org.hibernate.Session;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PersonDAO {
+    private static final PersonDAO INSTANCE = new PersonDAO();
+    private static final Logger logger = LoggerFactory.getLogger(PersonDAO.class);
+
+    public static PersonDAO getInstance() {
+        return INSTANCE;
+    }
+
+    public static void create(Person person) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        session.beginTransaction();
+        session.save(person);
+        session.getTransaction().commit();
+    }
+
+    public static void update(Person person, Long id) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        session.beginTransaction();
+        person.setId(id);
+        session.update(person);
+        session.getTransaction().commit();
+    }
+
+    public static void delete(Long id) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        session.beginTransaction();
+        Person person = new Person();
+        person.setId(id);
+        session.delete(person);
+        session.getTransaction().commit();
+    }
+
+    public static void read(Long id) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        session.beginTransaction();
+        Person person = new Person();
+        person.setId(id);
+        logger.info(session.load(Person.class, id).toString());
+        session.getTransaction().commit();
+    }
+}

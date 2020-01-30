7d45d71e2eea030ec0736afb4fd3cbd28b60e637
Kingl88 <King1375698l>
2020-01-30 12:28:54 +0300

    All relations

--- project-parent/common-hibernate/src/main/java/by/it/academy/design_bureau/App.java
+++ project-parent/common-hibernate/src/main/java/by/it/academy/design_bureau/App.java
@@ -1,25 +1,23 @@
 package by.it.academy.design_bureau;
 
+import by.it.academy.design_bureau.entity.Department;
 import by.it.academy.design_bureau.entity.Employee;
+import by.it.academy.design_bureau.entity.Meeting;
 import by.it.academy.design_bureau.util.HibernateUtil;
 import org.hibernate.Session;
 
 import java.time.LocalDateTime;
-
-/**
- * Hello world!
- */
+import java.util.ArrayList;
 public class App {
     public static void main(String[] args) {
         Session session = HibernateUtil.getSessionFactory().openSession();
         session.beginTransaction();
-        Employee e =
-                new Employee(null, "Ivan", "Ivanov", LocalDateTime.now(), null);
-        EmployeeDetail detail =
-                new EmployeeDetail(null, "Street", "city", "state", "country", null);
-        e.setEmployeeDetail(detail);
-        detail.setEmployee(e);
-        session.save(e);
+        Employee employee = new Employee(null, "Ivan", "Ivanov",LocalDateTime.now(), null, null, new ArrayList<>());
+        Meeting meeting = new Meeting("Hibernate relation grooming");
+        meeting.getEmployees().add(employee);
+        session.save(meeting);
+        employee.getMeetings().add(meeting);
+        session.saveOrUpdate(employee);
         session.getTransaction().commit();
         session.close();
         HibernateUtil.shutdown();
--- project-parent/common-hibernate/src/main/java/by/it/academy/design_bureau/entity/Employee.java
+++ project-parent/common-hibernate/src/main/java/by/it/academy/design_bureau/entity/Employee.java
@@ -2,12 +2,13 @@ package by.it.academy.design_bureau.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Generated;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import static javax.persistence.GenerationType.IDENTITY;
 
@@ -22,11 +23,24 @@ public class Employee {
     @Column(name = "EMPLOYEE_ID", unique = true)
     @Access(AccessType.PROPERTY)
     private Long employeeId;
+    @Column(name = "FIRST_NAME")
     private String firstName;
+    @Column(name = "LAST_NAME")
     private String lastName;
     private LocalDateTime date;
+
+    @OneToOne(fetch = FetchType.LAZY, mappedBy = "employee"
+            , cascade = CascadeType.ALL)
+    @Access(AccessType.PROPERTY)
+    private EmployeeDetail employeeDetail;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "DEPARTMENT_ID")
     private Department department;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "EMPLOYEE_MEETING",
+            joinColumns = {@JoinColumn(name = "EMPLOYEE_ID")},
+            inverseJoinColumns = {@JoinColumn(name = "MEETING_ID")})
+    private List<Meeting> meetings = new ArrayList<>();
 }
